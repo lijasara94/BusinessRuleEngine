efad4616c3ab666e248d8bc59c68089cfdf129be
lijasara94 <sonu.wilson@operationalsolutions.co.uk>
2021-12-12 12:01:53 +0000

    Arranged folder structure

--- BusinessRuleEngine/Products/PhysicalProduct.cs
+++ BusinessRuleEngine/Products/PhysicalProduct.cs
@@ -1,11 +1,5 @@
-﻿using BusinessRuleEngine;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace BusinessRuleEngine
+﻿
+namespace BusinessRuleEngine.Products
 {
     public class PhysicalProduct : Product, IGeneratePackingSlip, IGenerateAgentCommission
     {
