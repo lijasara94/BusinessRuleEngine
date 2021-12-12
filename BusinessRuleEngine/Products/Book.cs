45f584fd35a42b08d2af536a41d93a1f587d46c4
lijasara94 <sonu.wilson@operationalsolutions.co.uk>
2021-12-12 12:46:03 +0000

    Product Book is added with unit test

---
+++ BusinessRuleEngine/Products/Book.cs
@@ -0,0 +1,29 @@
+﻿using BusinessRuleEngine.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRuleEngine.Products
+{
+    public class Book : PhysicalProduct, IGeneratePackingSlipForRoyaltyDepartment
+    {
+        public Book()
+        {
+            Name = "Book";
+            Description = "Book";
+        }
+        public override Product FulfilProductOrder()
+        {
+            base.FulfilProductOrder();
+            GeneratePackingSlipForRoyaltyDepartment();
+            return this;
+        }
+        public void GeneratePackingSlipForRoyaltyDepartment()
+        {
+            IsPackingSlipGenerated_RoyaltyDepartment = true;
+            ProductPaymentActions.Add("Generate Packaging Slip for Royalty Department");
+        }
+    }
+}
--- BusinessRuleEngineTest/RuleEngineTest.cs
+++ BusinessRuleEngineTest/RuleEngineTest.cs
@@ -1,4 +1,4 @@
-using BusinessRuleEngine;
+using BusinessRuleEngine.Products;
 using Xunit;
 
 namespace BusinessRuleEngineTest
@@ -13,6 +13,7 @@ namespace BusinessRuleEngineTest
             PhysicalProduct physicalProduct = new PhysicalProduct();
             bool isAgentCommissionGenerated = true;
             bool isPackingSlipGenerated = true;
+            bool isPackingSlipGenerated_RoyaltyDept = false;
 
             //Act
 
@@ -22,8 +23,34 @@ namespace BusinessRuleEngineTest
             
             Assert.Equal(isPackingSlipGenerated,product.IsPackingSlipGenerated_Shipping);
             Assert.Equal(isAgentCommissionGenerated, product.IsAgentCommissionGenerated);
+            Assert.Equal(isPackingSlipGenerated_RoyaltyDept, product.IsPackingSlipGenerated_RoyaltyDepartment);
             Assert.Equal("Generate Packiing Slip for Shipping", product.ProductPaymentActions[0]);
             Assert.Equal("Generate Agent Commision",product.ProductPaymentActions[1]);
+
+        }
+
+        [Fact]
+        public void OrderBookTest()
+        {
+
+            //Arrange
+             Book book = new Book();
+            bool isAgentCommissionGenerated = true;
+            bool isPackingSlipGenerated = true;
+            bool isPackingSlipGenerated_RoyaltyDept = true;
+
+            //Act
+
+            var product = book.FulfilProductOrder();
+
+            //Assert
+
+            Assert.Equal(isPackingSlipGenerated, product.IsPackingSlipGenerated_Shipping);
+            Assert.Equal(isAgentCommissionGenerated, product.IsAgentCommissionGenerated);
+            Assert.Equal(isPackingSlipGenerated_RoyaltyDept, product.IsPackingSlipGenerated_RoyaltyDepartment);
+            Assert.Equal("Generate Packiing Slip for Shipping", product.ProductPaymentActions[0]);
+            Assert.Equal("Generate Agent Commision", product.ProductPaymentActions[1]);
+            Assert.Equal("Generate Packaging Slip for Royalty Department", product.ProductPaymentActions[2]);
 
         }
     }
