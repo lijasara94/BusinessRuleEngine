82f771cd325ecb15db799db7ed2bd140b2c1ab0f
lijasara94 <sonu.wilson@operationalsolutions.co.uk>
2021-12-12 12:56:54 +0000

    created email service and member ship class.

---
+++ BusinessRuleEngine/Products/MemberShip.cs
@@ -0,0 +1,22 @@
+﻿using BusinessRuleEngine.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRuleEngine.Products
+{
+    public class MemberShip : Product
+    {
+        private IEmailService emailService;
+        public MemberShip(IEmailService emailService)
+        {
+            this.emailService = emailService;
+        }
+        public override Product FulfilProductOrder()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BusinessRuleEngine/Services/EmailService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRuleEngine.Services
+{
+    public class EmailService : IEmailService
+    {
+        public void SendEmail(string message, string toAddress)
+        {
+            // Code to send the email
+        }
+    }
+}
