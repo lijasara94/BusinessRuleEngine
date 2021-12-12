c66bbe46dfb43a30231d8fad5e37457b90899866
lijasara94 <sonu.wilson@operationalsolutions.co.uk>
2021-12-12 11:22:49 +0000

    Added IoderFulminet and PhysicalProduct

---
+++ BusinessRuleEngine/PhysicalProduct.cs
@@ -0,0 +1,28 @@
+﻿using BusinessRuleEngine.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRuleEngine
+{
+    public class PhysicalProduct : IOrderFulfilment, IGeneratePackingSlip, IGenerateAgentCommission
+    {
+        public void FulfilOrder()
+        {
+            GeneratePackingSlip();
+            GenerateAgentCommision();
+        }
+
+        public void GenerateAgentCommision()
+        {
+            Console.WriteLine("Generate Agent Commision");
+        }
+
+        public void GeneratePackingSlip()
+        {
+            Console.WriteLine("Generate Packiing Slip for Shipping");
+        }
+    }
+}
