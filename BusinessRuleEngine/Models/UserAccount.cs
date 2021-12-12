f1c06c75a5914ab69c853b330891acd51dd782ae
lijasara94 <sonu.wilson@operationalsolutions.co.uk>
2021-12-12 13:06:15 +0000

    Added UserAccount Model

---
+++ BusinessRuleEngine/Models/UserAccount.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessRuleEngine.Models
+{
+    public class UserAccount
+    {
+        public string UserName  { get; set; }
+        public string EmailId { get; set; }
+
+        public bool IsUserActivated { get; set; }
+        public bool IsSentActivationMail { get; set; }
+
+        public bool IsUserAccountUpgraded { get; set; }
+        public bool IsSentAccountUpgradationMail { get; set; }
+    }
+}
